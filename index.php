bb619781387a3c9e3481e7b324d208fde7e857f5
Jaci14 <79876660+Jaci14@users.noreply.github.com>
2022-05-01 12:17:20 -0500

    Actualizacion posicion

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
   <body>
     <a-scene embedded arjs>
-      <a-entity scale=".1 .1 .1"> 
+      <a-entity rotation="0 0 0" position="0 0.0 0" scale=".1 .1 .1"> 
         <a-entity gltf-model="https://cdn.glitch.global/16b440ad-e892-4085-9aee-0cf4887d328f/octocatgit.glb?v=1648149096491"  scale="0.1 0.1 0.1" position="0 0 0" crossOrigin="anonymous">
         </a-entity>
         <a-marker-camera preset='hiro'></a-marker-camera>
